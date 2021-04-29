308367b1b9b68fa580f0025495e0cb140b9ce651
Lown Ignitus <lownignitus@gmail.com>
2021-04-28 20:09:32 -0700

    fixed output
    each for in loop reset output to ""
    fixed its location

--- custom scripts/sl_wqs.js
+++ custom scripts/sl_wqs.js
@@ -3,9 +3,9 @@ function weapons() {
   var wowhead_stuff = UrlFetchApp.fetch("https://www.wowhead.com/world-quests/sl/na").getContentText();
   var profs = ["dmgmax1", "Alchemy", "Blacksmithing", "Cooking", "Enchanting", "Engineering", "Fishing", "Herbalism", "Inscription", "Jewelcrafting", "Leatherworking", "Mining", "Skinning", "Tailoring"];
   var property;
-  
+  var output = "";
+
   for(property in profs) {
-    var output = "";
     for(var count=-1, index=-2; index != -1; count++, index=wowhead_stuff.indexOf(profs[property],index+1) );
       if(count > 0) {
         switch(profs[property]){
